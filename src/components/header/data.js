0083ef918289a793f3aca7c4cb005ec009ef684f
Hiiragi0831 <glazunovden@yandex.ru>
2023-11-09 18:51:32 +0300

    header

---
+++ src/components/header/data.js
@@ -0,0 +1,26 @@
+const data = [
+	{
+		link: 'Главная',
+	},
+	{
+		link: 'Услуги',
+		dropdown: [
+			{
+				link: 'Остеопатия',
+			},
+			{
+				link: 'Хиджама',
+			},
+			{
+				link: 'Для милых дам',
+				dropdown: [
+					{
+						link: 'Парикмахерский зал',
+					},
+				],
+			},
+		],
+	},
+];
+
+export default data;
